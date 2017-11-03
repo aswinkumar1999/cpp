6f9fa184a63485f7f6b7376a88040f2229da37f1
aswinkumar99 <aswinkumar1999@gmail.com>
2017-11-03 21:07:00 +0530

    No message

--- my1cpp.cpp
+++ my1cpp.cpp
@@ -2,12 +2,18 @@
 using namespace std;
 //Commenting .  .
 int main(){
-	int a,b;
-	cin>>a>>b;
-	cout<<a<<"\t"<<b<<endl;
-	a=a+b;
-	b=a-b;
-	a=a-b;
-	cout<<a<<"\t"<<b<<endl;
+	int a[100],n;
+	cin>>n;
+	for(int i=0;i<n;i++){
+	cin>>a[i];
+	}
+	for(int j=0;j<n/2;j++){
+	a[j]=a[j]+a[n-1-j];
+	a[n-1-j]=a[j]-a[n-1-j];
+	a[j]=a[j]-a[n-1-j];
+	}
+	for(int i=0;i<n;i++){
+	cout<<a[i]<<"\t";
+	}
 	return 0;
 }
